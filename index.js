1b8a732c7874a5f10f67565818a1001cc9be369a
MohammadHasan Pourghannad <mhpourghannad@gmail.com>
2023-12-12 09:18:51 +0330

    Add renderDemoCode function

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 const canvas = document.querySelector("canvas");
+const renderDemoCode = () => {
+  document.querySelector(".demo-container code").innerHTML = `${canvas.outerHTML
+    .toString()
+    .replace("<", "‌<‌")}‌`;
+};
 document.addEventListener("DOMContentLoaded", () => {
   dopDots(document.querySelector("main canvas").getContext("2d"));
-  const canvasDemo = `<‌canvas width="300px" height="300px" data-margin="10" data-type="grid" data-elementsize="3" data-color="#6082dd"
-                  data-elementformousemove=".canvas-wrapper" data-moveradius="25" data-secondColor="#ffffff"></‌canvas>`;
-  document.querySelector(".demo-container code").innerHTML = canvasDemo;
+  renderDemoCode();
 });
 document
   .querySelector(".element-size-input")
   .addEventListener("input", (event) => {
     canvas.dataset.elementsize = event.target.value;
     dopDots(document.querySelector("main canvas").getContext("2d"));
+    renderDemoCode();
   });
 document.querySelector(".margin-input").addEventListener("input", (event) => {
   canvas.dataset.margin = event.target.value;
   dopDots(document.querySelector("main canvas").getContext("2d"));
+  renderDemoCode();
 });
 document.querySelector(".move-radius").addEventListener("input", (event) => {
   canvas.dataset.moveradius = event.target.value;
   dopDots(document.querySelector("main canvas").getContext("2d"));
+  renderDemoCode();
 });
 
 document.querySelector(".second-color").addEventListener("input", (event) => {
   canvas.dataset.secondcolor = event.target.value;
   dopDots(document.querySelector("main canvas").getContext("2d"));
+  renderDemoCode();
 });
