04062790ba38391925009ca0b0102a806c1eaf78
MohammadHasan Pourghannad <mhpourghannad@gmail.com>
2023-12-12 08:49:54 +0330

    add <code>

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const canvas = document.querySelector("canvas");
 document.addEventListener("DOMContentLoaded", () => {
   dopDots(document.querySelector("main canvas").getContext("2d"));
+  const canvasDemo = `<‌canvas width="300px" height="300px" data-margin="10" data-type="grid" data-elementsize="3" data-color="#6082dd"
+                  data-elementformousemove=".canvas-wrapper" data-moveradius="25" data-secondColor="#ffffff"></‌canvas>`;
+  document.querySelector(".demo-container code").innerHTML = canvasDemo;
 });
 document
   .querySelector(".element-size-input")
